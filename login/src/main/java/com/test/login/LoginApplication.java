2bb27077b8a70ab50949eb15ddfb6900e8009cf1
Administrator <Administrator@YLMF-1704081533>
2018-04-12 00:31:32 +0800

    注释

--- login/src/main/java/com/test/login/LoginApplication.java
+++ login/src/main/java/com/test/login/LoginApplication.java
@@ -12,12 +12,12 @@ import org.springframework.context.annotation.ComponentScan;
  * 使用keytool创建数字证书
  * c:\\users\Alan>keytool -genkey -keystore keystore.jks -alias tycoonclent -keyalg RSA
  * 此sso-demo工程分
- * 数据管理模块 -mysql
+ * 数据管理模块 -mysql-作为jar包被引用
  * 安全配置模块-security
- * 登录认证模块-login
+ * 登录认证模块-login-包含mysql
  * 共享资源模块-resource
- * 客户端应用-web1
- * 客户端应用-web2
+ * 客户端应用-web1-包含msyql
+ * 客户端应用-web2-包含mysql
  * @author mhs123
  */
 @SpringBootApplication
