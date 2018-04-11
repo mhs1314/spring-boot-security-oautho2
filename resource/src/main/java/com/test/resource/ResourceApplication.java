ca77d26f9ab1d0dc1fe5ad248e2ef7163d59c967
Administrator <Administrator@YLMF-1704081533>
2018-04-11 23:33:15 +0800

    添加注释

--- resource/src/main/java/com/test/resource/ResourceApplication.java
+++ resource/src/main/java/com/test/resource/ResourceApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-
+/**
+ * 共享资源模块
+ * EnableResourceServer 标注这个应用是一个资源服务器
+ * 一个应用被标注为资源服务器后，在浏览器中就不能直接访问，如果在浏览器访问会出现xml提示
+ * 在客户端使用资源服务器的共享信息，可以使用spring-cloud-zuul提供的一个路由服务来实现
+ * @author mhs123
+ *
+ */
 @SpringBootApplication
+//EnableResourceServer 标注这个应用是一个资源服务器
 @EnableResourceServer
 @ComponentScan(basePackages = "com.test")
 public class ResourceApplication {
--- resource/src/main/java/com/test/resource/controller/UserController.java
+++ resource/src/main/java/com/test/resource/controller/UserController.java
@@ -9,12 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
-
+/**
+ * 这是一个共享用户信息的接口设计
+ * @author mhs123
+ *
+ */
 @RestController
 public class UserController {
     @Autowired
     private UserRepository userRepository;
-
+    //程序中通过Principal取得登录用户的用户名，然后通过用户名在数据库中查出用户的详细信息，最后返回用户信息的一个Map对象
     @RequestMapping("/user")
     public Map<String, Object> user(Principal puser) {
         User user = userRepository.findByName(puser.getName());
